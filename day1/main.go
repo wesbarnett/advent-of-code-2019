3cb6573289f53579e7d1a16fe12290309383856e
Wes Barnett <wes@barnettphd.com>
2022-10-21 21:33:17 -0400

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -9,8 +9,13 @@ import (
 	"strings"
 )
 
+func calc_fuel(mass int) int {
+	return int(math.Max(math.Floor(float64(mass)/3)-2, 0))
+}
+
 func main() {
 	var file string
+	var masses = []int{}
 	flag.StringVar(&file, "infile", "input", "Input file")
 	flag.Parse()
 
@@ -20,17 +25,29 @@ func main() {
 	}
 
 	lines := strings.Split(string(content), "\n")
-	fuel := 0
+	lines = lines[:len(lines)-1]
 	for _, line := range lines {
-		if len(line) == 0 {
-			break
-		}
 		mass, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fuel = fuel + int(math.Floor(float64(mass)/3)-2)
+		masses = append(masses, mass)
 	}
 
+	fuel := 0
+	for _, mass := range masses {
+		fuel = fuel + calc_fuel(mass)
+	}
 	log.Printf("%d", fuel)
+
+	total_fuel := 0
+	for _, mass := range masses {
+		for mass > 0 {
+			fuel = calc_fuel(mass)
+			total_fuel = total_fuel + fuel
+			mass = fuel
+		}
+		total_fuel = total_fuel + fuel
+	}
+	log.Printf("%d", total_fuel)
 }
