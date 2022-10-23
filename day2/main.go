3f99332e986e2b0719c54527d7bc5cc77efe46fd
Wes Barnett <wes@barnettphd.com>
2022-10-23 15:55:34 -0400

    refactor

--- day2/main.go
+++ day2/main.go
@@ -26,16 +26,35 @@ func read_program(file string) []int {
 	return program
 }
 
+func find_noun_and_verb(program []int, target int) (int, int) {
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			program[1] = noun
+			program[2] = verb
+			result := run_intcode_program(program)
+			if result == target {
+				return noun, verb
+			}
+
+		}
+	}
+	return -1, -1
+}
+
 func run_intcode_program(program []int) int {
 	i := 0
 	for {
 		v := program[i]
+		target := program[i+3]
+		if target > len(program) || target < 0 {
+			return -1
+		}
 		if v == 1 {
-			program[program[i+3]] = program[program[i+1]] + program[program[i+2]]
+			program[target] = program[program[i+1]] + program[program[i+2]]
 		} else if v == 2 {
-			program[program[i+3]] = program[program[i+1]] * program[program[i+2]]
+			program[target] = program[program[i+1]] * program[program[i+2]]
 		} else if v == 99 {
-			break
+			return program[0]
 		}
 		i = i + 4
 	}
@@ -50,4 +69,7 @@ func main() {
 	program := read_program(file)
 	result := run_intcode_program(program)
 	fmt.Println(result)
+
+	noun, verb := find_noun_and_verb(program, 19690720)
+	fmt.Println(noun, verb)
 }
